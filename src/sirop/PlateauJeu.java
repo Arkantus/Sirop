fd9a6ac7aa0aa948b33dd7490f91cc8b118fca72
IAZERTYUIOPI <IAZERTYUIOPI@130.66.204.135>
2014-09-15 11:06:39 +0200

    j'aime le pâté

--- src/sirop/PlateauJeu.java
+++ src/sirop/PlateauJeu.java
@@ -38,5 +38,83 @@ public class PlateauJeu {
         this.obstacleList.add(o);
     }
     
+    public void moveRobot(int direction, Robot r)
+    {
+       int deltax = 0;
+       int deltay = 0;
+        
+        switch(direction){
+            case 0: 
+                deltax = 1;
+                deltay = 0;
+                break;
+            case 1: 
+                deltax = 1;
+                deltay = 1;
+                break;
+            case 2: 
+                deltax = 0;
+                deltay = 1;
+                break;
+            case 3: 
+                deltax = -1;
+                deltay = 1;
+                break;
+            case 4: 
+                deltax = -1;
+                deltay = 0;
+                break;
+            case 5: 
+                deltax = -1;
+                deltay = -1;
+                break;    
+            case 6: 
+                deltax = 0;
+                deltay = -1;
+                break;    
+            case 7: 
+                deltax = 1;
+                deltay = -1;
+                break;
+        }
+            
+        int newxPos = r.getxPos() + deltax;
+        int newyPos = r.getyPos() + deltay;
+        
+        boolean isInPlateau = newxPos>=0 && newyPos >=0 && newxPos < largeur && newyPos < hauteur;
+        boolean isOnObstacle = false;
+        for (Obstacle o : obstacleList) 
+        {
+            if(o.getxPos() == newxPos && o.getyPos() == newyPos)
+            {
+                isOnObstacle = true;
+                break;
+            }
+        }
+        
+        boolean hasEnoughEnergy = r.getEnergy()>0;
+        
+        if(isInPlateau && !isOnObstacle && hasEnoughEnergy)
+            {
+                r.depenserEnergie(1);
+                r.setxPos(newxPos);
+                r.setyPos(newyPos);
+            }  
+        else
+            {
+                System.out.println("Move failure");
+            }
+    }
     
+    public void displayPlateau()
+    {
+        for (Obstacle o : obstacleList) 
+        {
+            System.out.println(o.toString());
+        }
+        for (Robot r : robotList) 
+        {
+            System.out.println(r.toString());
+        }
+    }
 }
