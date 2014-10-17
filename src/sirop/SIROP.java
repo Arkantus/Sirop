6994c6cff61fc8368ceac19f5cff81889974261b
IAZERTYUIOPI <IAZERTYUIOPI@130.66.207.165>
2014-10-17 16:50:10 +0200

    Merge origin/master
    Conflicts:
    	src/sirop/SIROP.java

--- src/sirop/SIROP.java
+++ src/sirop/SIROP.java
@@ -56,7 +56,6 @@ public class SIROP {
         
         while(keepPlaying)
         {
-            plateau.saveState(cnp + "_Save_");
             if (!rF.getIsPaused()){
             //System.out.println(plateau.toString());
             Thread.sleep(500);
